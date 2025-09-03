4c81ae8c2ddb87445efddd75f4bd47f80d50e41c
Ильдар Галиев <nb1mvn@yandex.ru>
2025-09-03 20:56:10 +0400

    Третья проверка

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -1,84 +1,115 @@
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
+from django.test import TestCase
 from django.urls import reverse
 
-from notes.tests.conftest import BaseNoteList
+from notes.models import Note
+
+User = get_user_model()
 
 
-class TestRoutes(BaseNoteList):
+class TestRoutes(TestCase):
 
-    def test_pages_status_codes(self):
-        """Тест статус кодов для разных пользователей и страниц"""
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='testuser',
+                                            password='123123')
+        cls.other_user = User.objects.create_user(username='otheruser',
+                                                  password='321321')
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Текст',
+            author=cls.user,
+            slug='test-note'
+        )
+
+    def test_pages_availability_for_different_users(self):
+        """Тест доступности страниц для разных типов пользователей."""
         test_cases = [
-            # Анонимный пользователь
-            (None, 'notes:home', None, HTTPStatus.OK),
-            (None, 'users:login', None, HTTPStatus.OK),
-            (None, 'users:signup', None, HTTPStatus.OK),
-            (None, 'notes:list', None, HTTPStatus.FOUND),
-            # redirect to login
-            (None, 'notes:add', None, HTTPStatus.FOUND),
-            # redirect to login
-            (None, 'notes:success', None, HTTPStatus.FOUND),
-            # redirect to login
-            (None, 'notes:detail', {'slug': self.note.slug}, HTTPStatus.FOUND),
-            # redirect
-            (None, 'notes:edit', {'slug': self.note.slug}, HTTPStatus.FOUND),
-            # redirect
-            (None, 'notes:delete', {'slug': self.note.slug}, HTTPStatus.FOUND),
-            # redirect
-
-            # Авторизованный пользователь (не автор)
-            (self.reader, 'notes:list', None, HTTPStatus.OK),
-            (self.reader, 'notes:add', None, HTTPStatus.OK),
-            (self.reader, 'notes:success', None, HTTPStatus.OK),
-            (self.reader, 'notes:detail', {'slug': self.note.slug},
-             HTTPStatus.NOT_FOUND),
-            (self.reader, 'notes:edit', {'slug': self.note.slug},
-             HTTPStatus.NOT_FOUND),
-            (self.reader, 'notes:delete', {'slug': self.note.slug},
-             HTTPStatus.NOT_FOUND),
-
+            # Анонимные пользователи
+            {
+                'user': None,
+                'urls': [
+                    ('notes:home', None, HTTPStatus.OK),
+                    ('users:login', None, HTTPStatus.OK),
+                    ('users:signup', None, HTTPStatus.OK),
+                ]
+            },
+            # Авторизованные пользователи (общие страницы)
+            {
+                'user': self.user,
+                'urls': [
+                    ('notes:list', None, HTTPStatus.OK),
+                    ('notes:add', None, HTTPStatus.OK),
+                    ('notes:success', None, HTTPStatus.OK),
+                ]
+            },
             # Автор заметки
-            (self.author, 'notes:list', None, HTTPStatus.OK),
-            (self.author, 'notes:add', None, HTTPStatus.OK),
-            (self.author, 'notes:success', None, HTTPStatus.OK),
-            (self.author, 'notes:detail', {'slug': self.note.slug},
-             HTTPStatus.OK),
-            (self.author, 'notes:edit', {'slug': self.note.slug},
-             HTTPStatus.OK),
-            (self.author, 'notes:delete', {'slug': self.note.slug},
-             HTTPStatus.OK),
+            {
+                'user': self.user,
+                'urls': [
+                    ('notes:detail', {'slug': self.note.slug}, HTTPStatus.OK),
+                    ('notes:edit', {'slug': self.note.slug}, HTTPStatus.OK),
+                    ('notes:delete', {'slug': self.note.slug}, HTTPStatus.OK),
+                ]
+            },
+            # Другой пользователь (не автор)
+            {
+                'user': self.other_user,
+                'urls': [
+                    ('notes:detail', {'slug': self.note.slug},
+                     HTTPStatus.NOT_FOUND),
+                    ('notes:edit', {'slug': self.note.slug},
+                     HTTPStatus.NOT_FOUND),
+                    ('notes:delete', {'slug': self.note.slug},
+                     HTTPStatus.NOT_FOUND),
+                ]
+            }
         ]
 
-        for user, url_name, kwargs, expected_status in test_cases:
-            with self.subTest(user=user.username if user
-                              else 'anonymous', url=url_name):
-                # Авторизуем пользователя если указан
-                if user:
-                    self.client.force_login(user)
-                else:
-                    self.client.logout()
+        for case in test_cases:
+            if case['user']:
+                self.client.force_login(case['user'])
+            else:
+                self.client.logout()
 
-                url = reverse(url_name, kwargs=kwargs)
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, expected_status)
+            for name, kwargs, expected_status in case['urls']:
+                with self.subTest(user=case['user'], name=name, kwargs=kwargs):
+                    url = reverse(name, kwargs=kwargs)
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, expected_status)
 
-    def test_redirects_for_anonymous_user(self):
-        """Тест редиректов для анонимного пользователя"""
-        redirect_cases = [
-            # URL с параметром slug
+    def test_redirect_for_anonymous_client(self):
+        """Тест редиректов для анонимных пользователей."""
+        login_url = reverse('users:login')
+
+        # URL с параметрами (требуют slug)
+        note_urls = [
             ('notes:detail', {'slug': 'test-slug'}),
             ('notes:edit', {'slug': 'test-slug'}),
             ('notes:delete', {'slug': 'test-slug'}),
-            # URL без параметров
+        ]
+
+        # URL без параметров
+        simple_urls = [
             ('notes:add', None),
             ('notes:success', None),
             ('notes:list', None),
         ]
 
-        for url_name, kwargs in redirect_cases:
-            with self.subTest(url=url_name):
-                url = reverse(url_name, kwargs=kwargs)
-                expected_redirect = f"{self.LOGIN_URL}?next={url}"
+        # Тестируем URL с параметрами
+        for name, kwargs in note_urls:
+            with self.subTest(name=name):
+                url = reverse(name, kwargs=kwargs)
+                redirect_url = f"{login_url}?next={url}"
                 response = self.client.get(url)
-                self.assertRedirects(response, expected_redirect)
+                self.assertRedirects(response, redirect_url)
+
+        # Тестируем URL без параметров
+        for name, kwargs in simple_urls:
+            with self.subTest(name=name):
+                url = reverse(name, kwargs=kwargs)
+                redirect_url = f"{login_url}?next={url}"
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
